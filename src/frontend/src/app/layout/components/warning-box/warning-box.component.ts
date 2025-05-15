c8c9773ab08c52531048a6e870587078e12449ad
Krystian Sitarek <krystiansitarek@gmail.com>
2025-05-15 19:46:05 +0200

    warning box refined

--- src/frontend/src/app/layout/components/warning-box/warning-box.component.ts
+++ src/frontend/src/app/layout/components/warning-box/warning-box.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'hb-warning-box',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './warning-box.component.html',
-  styleUrl: './warning-box.component.scss'
+  styleUrl: './warning-box.component.scss',
 })
-export class WarningBoxComponent {
-
-}
+export class WarningBoxComponent {}
